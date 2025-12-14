61d82ff854fc0647fc6e077fd801b8a3a26cd445
Your Namenicolasmaurizi <nicolasmaurizi@gmail.com>
2025-12-13 23:10:55 -0300

    refactor: update Vite configuration and add Vitest setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: "/design-patterns-playground/",
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/design-patterns-playground/" : "/",
   plugins: [react()],
-  test: {
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.ts",
-    globals: true,
-  },
-});
+}));
